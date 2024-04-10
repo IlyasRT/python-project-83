489beaf6513300de27006af8b0ddc4ba133f120f
IlyasRT <razyapovig@gmail.com>
2024-04-10 14:20:58 +0300

    page_analyzer2

--- 2.py
+++
@@ -1,3 +0,0 @@
-### Hexlet tests and linter status:
-hthrhhrh
-[![Actions Status](https://github.com/IlyasRT/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/IlyasRT/python-project-83/actions)
