6b94622f29f8deedd14959586cb86acce10c0e23
IlyasRT <razyapovig@gmail.com>
2024-05-10 09:45:41 +0300

    add some

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 # print('app.config[\'DATABASE_URL\']=', app.config['DATABASE_URL'])
+# conn = psycopg2.connect(DATABASE_URL)
+
+try:
+    # пытаемся подключиться к базе данных
+    conn = psycopg2.connect(app.config['DATABASE_URL'])
+except:
+    # в случае сбоя подключения будет выведено сообщение  в STDOUT
+    print('Can`t establish connection to database')
 
 
 @app.route('/')
@@ -15,4 +23,8 @@ def hello_world():
     # return 'Проверка --> Welcome to Flask!'
     # return render_template('index.html')
     return render_template('index2.html')
-    
+
+
+#  poetry run flask --app page_analyzer:app run
+#  poetry run gunicorn -w 5 -b 127.0.0.1:5000 page_analyzer:app
+#  python3 -m pip install --force-reinstall dist/*.whl
