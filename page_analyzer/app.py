707df1b27d72f77a2e527e88d7a413de6f1540cf
IlyasRT <razyapovig@gmail.com>
2024-05-10 09:16:50 +0300

    same commit

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,15 +3,11 @@ from dotenv import load_dotenv
 import os
 import psycopg2
 
-
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
-print('app=',app)
-print('app.config=',app.config)
-print('app.config[\'SECRET_KEY\']=', app.config['SECRET_KEY'])
-print('app.config[\'DATABASE_URL\']=', app.config['DATABASE_URL'])
+# print('app.config[\'DATABASE_URL\']=', app.config['DATABASE_URL'])
 
 
 @app.route('/')
