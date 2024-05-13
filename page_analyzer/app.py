d45c23f4aeb74f41a3c3a1e7b6fc00af75b2318c
IlyasRT <razyapovig@gmail.com>
2024-05-13 18:32:36 +0300

    add after connecting with database-1

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,22 +2,37 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 import psycopg2
+from urllib.parse import urlparse
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+#app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+#print('app.config[\'DATABASE_URL\']=', app.config['DATABASE_URL'])
+DATABASE_URL = os.getenv('DATABASE_URL')
+#print('DATABASE_URL=', DATABASE_URL)
+# print('DATABASE_URL=', DATABASE_URL)
+#db_url = urlparse(DATABASE_URL)
+#print('db_url=', db_url)
+# print(os.environ['KEY'])
+# print(os.getenv('KEY'))
+# print('app.config[\'SECRET_KEY\']=', app.config['SECRET_KEY'])
 # print('app.config[\'DATABASE_URL\']=', app.config['DATABASE_URL'])
-conn = psycopg2.connect(app.config['DATABASE_URL'])
-'''
+# conn = psycopg2.connect(app.config['DATABASE_URL'])
+
+
+# conn = psycopg2.connect(DATABASE_URL)
+# conn = psycopg2.connect('postgresql://user_page_analyzer:psw1@localhost:5432/db_page_analyzer_1')
+
+
 try:
     # пытаемся подключиться к базе данных
-    conn = psycopg2.connect(app.config['DATABASE_URL'])
+    conn = psycopg2.connect(DATABASE_URL)
     print('Establish connection to database')
 except:
     # в случае сбоя подключения будет выведено сообщение  в STDOUT
     print('Can`t establish connection to database')
-'''
+
 
 @app.route('/')
 def hello_world():
